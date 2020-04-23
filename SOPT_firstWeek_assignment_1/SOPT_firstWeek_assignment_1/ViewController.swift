2264108f096e7462482ea95dec90db73662fb978
i-colours-u <i.colours.u@gmail.com>
2020-04-23 15:37:54 +0900

    Navigation을 이용한 화면 이동 과제

---
+++ SOPT_firstWeek_assignment_1/SOPT_firstWeek_assignment_1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SOPT_firstWeek_assignment_1
+//
+//  Created by 송지훈 on 2020/04/23.
+//  Copyright © 2020 송지훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
