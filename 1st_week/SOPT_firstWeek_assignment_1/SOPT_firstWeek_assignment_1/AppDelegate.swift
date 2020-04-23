256f5137ab01a25a49954773a1a2de0a9fa8fe34
i-colours-u <i.colours.u@gmail.com>
2020-04-23 19:00:41 +0900

    네비게이션을 활용한 화면 전환

--- 1st_week/SOPT_firstWeek_assignment_1/SOPT_firstWeek_assignment_1/AppDelegate.swift
+++ 1st_week/SOPT_firstWeek_assignment_1/SOPT_firstWeek_assignment_1/AppDelegate.swift
@@ -3,7 +3,7 @@
 //  SOPT_firstWeek_assignment_1
 //
 //  Created by 송지훈 on 2020/04/23.
-//  Copyright © 2020 송지훈. All rights reserved.
+//  Copyright © 2020 송지훈. All rights reserved
 //
 
 import UIKit
