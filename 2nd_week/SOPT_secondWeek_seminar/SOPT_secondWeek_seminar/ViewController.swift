f6d3e6c796dfb06ad5656ec565661dc7a6f1cea3
i-colours-u <i.colours.u@gmail.com>
2020-04-24 14:55:19 +0900

    semianar file add

---
+++ 2nd_week/SOPT_secondWeek_seminar/SOPT_secondWeek_seminar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SOPT_secondWeek_seminar
+//
+//  Created by 송지훈 on 2020/04/24.
+//  Copyright © 2020 송지훈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
