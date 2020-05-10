6ff9f66e531920d0ac79f5a62d4ce575e68b97bc
i-colours-u <i.colours.u@gmail.com>
2020-05-10 20:39:17 +0900

    3주차 세미나 실습자료

--- 3rd_week/SOPT_thirdWeek_CollectionView/SOPT_thirdWeek_CollectionView/ViewController.swift
+++ 3rd_week/SOPT_thirdWeek_CollectionView/SOPT_thirdWeek_CollectionView/ViewController.swift
@@ -59,7 +59,7 @@ extension ViewController : UICollectionViewDelegateFlowLayout{
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        return UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
+        return UIEdgeInsets(top: 0, left: 5, bottom: 0, right: 5)
     }
     
     
